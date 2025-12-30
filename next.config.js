7a97fb095a64a4559613b22dbb7b2d112c29741c
Aliff Marwan <aliffmarwan@Aliffs-MacBook-Air.local>
2025-12-30 22:17:45 +0800

    fix: explicitly pass SENTRY_AUTH_TOKEN in next.config.js

--- next.config.js
+++ next.config.js
@@ -70,6 +70,9 @@ module.exports = withSentryConfig(module.exports, {
   org: "abangbob",
   project: "javascript-nextjs",
 
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
